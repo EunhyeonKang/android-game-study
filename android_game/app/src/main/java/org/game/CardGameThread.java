13d70dca8643b9b4418d2856cdebdca0136d5439
kangeunhyeon <rkddmsgus1@daum.net>
2021-02-02 18:53:26 +0900

    android_game

---
+++ android_game/app/src/main/java/org/game/CardGameThread.java
@@ -0,0 +1,27 @@
+package org.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.os.Looper;
+import android.widget.Toast;
+
+public class CardGameThread extends Thread {
+    CardGameView _cardGameView;
+
+    public CardGameThread(CardGameView _cardGameView) {
+        this._cardGameView = _cardGameView;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            _cardGameView.checkMatch();
+            if(_cardGameView.isMatchedAll()){
+                System.out.println("스레드 종료");
+                break;
+            }
+        }
+    }
+}
---
+++ android_game/app/src/main/java/org/game/MainActivity.java
@@ -0,0 +1,19 @@
+package org.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //이미지 파일을 불러와 화면에 그리기
+        setContentView(new CardGameView(this));
+    }
+}
